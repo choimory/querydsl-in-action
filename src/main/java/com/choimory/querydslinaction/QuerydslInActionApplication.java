92bf6bc821c74a27d3af714e9aa579a27b74e663
choimory <choimory@naver.com>
2021-06-12 00:30:56 +0900

    Initial commit

---
+++ src/main/java/com/choimory/querydslinaction/QuerydslInActionApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.querydslinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/querydslinaction/QuerydslInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.querydslinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
