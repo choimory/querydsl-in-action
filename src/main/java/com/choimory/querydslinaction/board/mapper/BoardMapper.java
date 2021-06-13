120d6647cf407a2fd9493564a81d9ba2a93b5b41
choimory <choimory@naver.com>
2021-06-13 21:45:59 +0900

    매퍼 작성

---
+++ src/main/java/com/choimory/querydslinaction/board/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.choimory.querydslinaction.board.mapper;
+
+import com.choimory.querydslinaction.board.dto.response.BoardResponseDto;
+import com.choimory.querydslinaction.board.entity.Board;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface BoardMapper {
+    BoardResponseDto toDto (Board entity);
+    List<BoardResponseDto> toDtos (List<Board> entities);
+    Page<BoardResponseDto> toDtos (Page<Board> entities);
+}
