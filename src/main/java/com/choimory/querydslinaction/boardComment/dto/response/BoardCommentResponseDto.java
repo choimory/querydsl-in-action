c76f39282e17a1bd986609d26dc0d254b8f3000b
choimory <choimory@naver.com>
2021-06-13 21:42:31 +0900

    dto 작성

---
+++ src/main/java/com/choimory/querydslinaction/boardComment/dto/response/BoardCommentResponseDto.java
@@ -0,0 +1,4 @@
+package com.choimory.querydslinaction.boardComment.dto.response;
+
+public class BoardCommentResponseDto {
+}
---
+++ src/main/java/com/choimory/querydslinaction/user/dto/response/UserResponseDto.java
@@ -0,0 +1,4 @@
+package com.choimory.querydslinaction.user.dto.response;
+
+public class UserResponseDto {
+}
